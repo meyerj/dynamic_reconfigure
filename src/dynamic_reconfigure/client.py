33970e943f3f2886d7504ad59344cd8de8868055
Mikael Arguedas <mikael.arguedas@gmail.com>
2017-03-08 22:44:35 -0800

    WIP python3 compat + style

--- src/dynamic_reconfigure/client.py
+++ src/dynamic_reconfigure/client.py
@@ -31,59 +31,62 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 """
-Python client API for dynamic_reconfigure (L{DynamicReconfigureClient}) as well as 
-example server implementation (L{DynamicReconfigureServer}).
+Python client API for dynamic_reconfigure (L{DynamicReconfigureClient})
+as well as example server implementation (L{DynamicReconfigureServer}).
 """
 
-from __future__ import with_statement
+from __future__ import print_function, with_statement
 
-try:
-    import roslib; roslib.load_manifest('dynamic_reconfigure')
-except:
-    pass
-import rospy
-import rosservice                  
 import sys
 import threading
 import time
-import types
+try:
+    import roslib
+    roslib.load_manifest('dynamic_reconfigure')
+except:
+    pass
 from dynamic_reconfigure import DynamicReconfigureParameterException
-from dynamic_reconfigure.srv import Reconfigure as ReconfigureSrv
+from dynamic_reconfigure.encoding import decode_config, decode_description
+from dynamic_reconfigure.encoding import encode_config, extract_params
 from dynamic_reconfigure.msg import Config as ConfigMsg
 from dynamic_reconfigure.msg import ConfigDescription as ConfigDescrMsg
-from dynamic_reconfigure.msg import IntParameter, BoolParameter, StrParameter, DoubleParameter, ParamDescription
-from dynamic_reconfigure.encoding import *
+from dynamic_reconfigure.srv import Reconfigure as ReconfigureSrv
+import rospy
+
 
 class Client(object):
     """
     Python dynamic_reconfigure client API
     """
-    def __init__(self, name, timeout=None, config_callback=None, description_callback=None):
+    def __init__(self, name, timeout=None,
+                 config_callback=None, description_callback=None):
         """
         Connect to dynamic_reconfigure server and return a client object
-        
+
         @param name: name of the server to connect to (usually the node name)
         @type  name: str
         @param timeout: time to wait before giving up
         @type  timeout: float
         @param config_callback: callback for server parameter changes
-        @param description_callback: internal use only as the API has not stabilized
+        @param description_callback: internal use only as the API may change
         """
-        self.name              = name
-        self.config            = None
+        self.name = name
+        self.config = None
         self.param_description = None
         self.group_description = None
-        
+
         self._param_types = None
 
         self._cv = threading.Condition()
 
-        self._config_callback      = config_callback
+        self._config_callback = config_callback
         self._description_callback = description_callback
 
-        self._set_service      = self._get_service_proxy('set_parameters', timeout)       
-        self._descriptions_sub = self._get_subscriber('parameter_descriptions', ConfigDescrMsg, self._descriptions_msg)
-        self._updates_sub      = self._get_subscriber('parameter_updates',      ConfigMsg,      self._updates_msg)
+        self._set_service = self._get_service_proxy('set_parameters', timeout)
+        self._descriptions_sub = self._get_subscriber(
+            'parameter_descriptions', ConfigDescrMsg, self._descriptions_msg)
+        self._updates_sub = self._get_subscriber(
+            'parameter_updates', ConfigMsg, self._updates_msg)
 
     def get_configuration(self, timeout=None):
         """
@@ -92,13 +95,14 @@ class Client(object):
 
         @param timeout: time to wait before giving up
         @type  timeout: float
-        @return: dictionary mapping parameter names to values or None if unable to retrieve config.
+        @return: dictionary mapping parameter names to values
+                 or None if unable to retrieve config.
         @rtype: {str: value}
         """
         if timeout is None or timeout == 0.0:
             if self.get_configuration(timeout=1.0) is None:
-                print >> sys.stderr, 'Waiting for configuration...'
-                
+                print('Waiting for configuration...', sys.stderr)
+
                 with self._cv:
                     while self.config is None:
                         if rospy.is_shutdown():
@@ -121,7 +125,7 @@ class Client(object):
         """
         UNSTABLE. Return a description of the parameters for the server.
         Do not use this method as the type that is returned may change.
-        
+
         @param timeout: time to wait before giving up
         @type  timeout: float
         """
@@ -168,7 +172,8 @@ class Client(object):
         """
         Change the server's configuration
 
-        @param changes: dictionary of key value pairs for the parameters that are changing
+        @param changes: dictionary of key value pairs for
+                        the parameters that are changing
         @type  changes: {str: value}
         """
         # Retrieve the parameter descriptions
@@ -181,28 +186,34 @@ class Client(object):
                 if name != 'groups':
                     dest_type = self._param_types.get(name)
                     if dest_type is None:
-                        raise DynamicReconfigureParameterException('don\'t know parameter: %s' % name)
-                
+                        raise DynamicReconfigureParameterException(
+                            'don\'t know parameter: %s' % name)
+
                     try:
                         found = False
-                        descr = [x for x in self.param_description if x['name'].lower() == name.lower()][0]
+                        descr = [x for x in self.param_description if
+                                 x['name'].lower() == name.lower()][0]
 
                         # Fix not converting bools properly
                         if dest_type is bool and type(value) is str:
-                            changes[name] = value.lower() in ("yes", "true", "t", "1")
+                            changes[name] = value.lower() in (
+                                "yes", "true", "t", "1")
                             found = True
                         # Handle enums
-                        elif type(value) is str and not descr['edit_method'] == '':
+                        elif isinstance(value, str):
+                            if descr['edit_method'] == '':
+                                break
                             enum_descr = eval(descr['edit_method'])
                             found = False
                             for const in enum_descr['enum']:
                                 if value.lower() == const['name'].lower():
-                                    val_type = self._param_type_from_string(const['type'])
+                                    val_type = self._param_type_from_string(
+                                        const['type'])
                                     changes[name] = val_type(const['value'])
                                     found = True
                         if not found:
                             if sys.version_info.major < 3:
-                                if type(value) is unicode:
+                                if isinstance(value, unicode):
                                     changes[name] = unicode(value)
                                 else:
                                     changes[name] = dest_type(value)
@@ -210,16 +221,18 @@ class Client(object):
                                 changes[name] = dest_type(value)
 
                     except ValueError as e:
-                        raise DynamicReconfigureParameterException('can\'t set parameter \'%s\' of %s: %s' % (name, str(dest_type), e))
+                        raise DynamicReconfigureParameterException(
+                            "can't set parameter '%s' of %s: %s" % (
+                                name, str(dest_type), e))
 
         if 'groups' in changes.keys():
             changes['groups'] = self.update_groups(changes['groups'])
 
         config = encode_config(changes)
-        msg    = self._set_service(config).config
+        msg = self._set_service(config).config
         if self.group_description is None:
             self.get_group_descriptions()
-        resp   = decode_config(msg, self.group_description)
+        resp = decode_config(msg, self.group_description)
 
         return resp
 
@@ -227,21 +240,21 @@ class Client(object):
         """
         Changes the servers group configuration
 
-        @param changes: dictionary of key value pairs for the parameters that are changing
+        @param changes: dictionary of key value pairs for
+                        the parameters that are changing
         @type  changes: {str: value}
         """
-        
+
         descr = self.get_group_descriptions()
 
-        groups = []
         def update_state(group, description):
-            for p,g in description['groups'].items():
+            for p, g in description['groups'].items():
                 if g['name'] == group:
                     description['groups'][p]['state'] = changes[group]
                 else:
                     update_state(group, g)
             return description
- 
+
         for change in changes:
             descr = update_state(change, descr)
 
@@ -254,7 +267,7 @@ class Client(object):
         self._descriptions_sub.unregister()
         self._updates_sub.unregister()
 
-    ## config_callback
+    # config_callback
 
     def get_config_callback(self):
         """
@@ -272,8 +285,7 @@ class Client(object):
 
     config_callback = property(get_config_callback, set_config_callback)
 
-    ## description_callback        
-
+    # description_callback
     def get_description_callback(self):
         """
         Get the current description_callback
@@ -289,17 +301,18 @@ class Client(object):
         if self._description_callback is not None:
             self._description_callback(self.param_description)
 
-    description_callback = property(get_description_callback, set_description_callback)
+    description_callback = property(
+        get_description_callback, set_description_callback)
 
     # Implementation
-
     def _get_service_proxy(self, suffix, timeout):
         service_name = rospy.resolve_name(self.name + '/' + suffix)
         if timeout is None or timeout == 0.0:
             try:
                 rospy.wait_for_service(service_name, 1.0)
             except rospy.exceptions.ROSException:
-                print >> sys.stderr, 'Waiting for service %s...' % service_name
+                print('Waiting for service %s...' % service_name,
+                      file=sys.stderr)
                 rospy.wait_for_service(service_name, timeout)
         else:
             rospy.wait_for_service(service_name, timeout)
@@ -308,14 +321,14 @@ class Client(object):
 
     def _get_subscriber(self, suffix, type, callback):
         topic_name = rospy.resolve_name(self.name + '/' + suffix)
-        
+
         return rospy.Subscriber(topic_name, type, callback=callback)
 
     def _updates_msg(self, msg):
         if self.group_description is None:
             self.get_group_descriptions()
         self.config = decode_config(msg, self.group_description)
-        
+
         with self._cv:
             self._cv.notifyAll()
         if self._config_callback is not None:
@@ -338,9 +351,15 @@ class Client(object):
             self._description_callback(self.param_description)
 
     def _param_type_from_string(self, type_str):
-        if   type_str == 'int':    return int
-        elif type_str == 'double': return float
-        elif type_str == 'str':    return str
-        elif type_str == 'bool':   return bool
+        if type_str == 'int':
+            return int
+        elif type_str == 'double':
+            return float
+        elif type_str == 'str':
+            return str
+        elif type_str == 'bool':
+            return bool
         else:
-            raise DynamicReconfigureParameterException('parameter has unknown type: %s. This is a bug in dynamic_reconfigure.' % type_str)
+            raise DynamicReconfigureParameterException(
+                'parameter has unknown type: %s. This is a bug in' +
+                'dynamic_reconfigure.' % type_str)
--- src/dynamic_reconfigure/server.py
+++ src/dynamic_reconfigure/server.py
@@ -31,8 +31,8 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 """
-Python client API for dynamic_reconfigure (L{DynamicReconfigureClient}) as well as 
-example server implementation (L{DynamicReconfigureServer}).
+Python client API for dynamic_reconfigure (L{DynamicReconfigureClient})
+as well as example server implementation (L{DynamicReconfigureServer}).
 """
 
 from __future__ import with_statement
@@ -41,17 +41,19 @@ try:
     import roslib; roslib.load_manifest('dynamic_reconfigure')
 except:
     pass
-import rospy
-import rosservice                  
-import threading
-import time
 import copy
+import threading
+
 from dynamic_reconfigure import DynamicReconfigureCallbackException
-from dynamic_reconfigure.srv import Reconfigure as ReconfigureSrv
+from dynamic_reconfigure.encoding import decode_config, encode_config
+from dynamic_reconfigure.encoding import encode_description, extract_params
+from dynamic_reconfigure.encoding import get_tree, initial_config
 from dynamic_reconfigure.msg import Config as ConfigMsg
 from dynamic_reconfigure.msg import ConfigDescription as ConfigDescrMsg
-from dynamic_reconfigure.msg import IntParameter, BoolParameter, StrParameter, DoubleParameter, ParamDescription
-from dynamic_reconfigure.encoding import *
+from dynamic_reconfigure.srv import Reconfigure as ReconfigureSrv
+
+import rospy
+
 
 class Server(object):
     def __init__(self, type, callback, namespace=""):
@@ -69,31 +71,40 @@ class Server(object):
         self.description = encode_description(type)
         self._copy_from_parameter_server()
         self.callback = callback
-        self._clamp(self.config) 
+        self._clamp(self.config)
 
         # setup group defaults
         self.config['groups'] = get_tree(self.description)
-        self.config = initial_config(encode_config(self.config), type.config_description)
+        self.config = initial_config(
+            encode_config(self.config), type.config_description)
 
-        self.descr_topic = rospy.Publisher(self.ns + 'parameter_descriptions', ConfigDescrMsg, latch=True, queue_size=10)
-        self.descr_topic.publish(self.description);
+        self.descr_topic = rospy.Publisher(
+            self.ns + 'parameter_descriptions', ConfigDescrMsg,
+            latch=True, queue_size=10)
+        self.descr_topic.publish(self.description)
 
-        self.update_topic = rospy.Publisher(self.ns + 'parameter_updates', ConfigMsg, latch=True, queue_size=10)
-        self._change_config(self.config, ~0) # Consistent with the C++ API, the callback gets called with level=~0 (i.e. -1)
+        self.update_topic = rospy.Publisher(
+            self.ns + 'parameter_updates', ConfigMsg, latch=True, queue_size=10)
+        # For consistency with C++ API,
+        # the callback gets called with level=~0 (i.e. -1)
+        self._change_config(self.config, ~0)
 
-        self.set_service = rospy.Service(self.ns + 'set_parameters', ReconfigureSrv, self._set_callback)
+        self.set_service = rospy.Service(
+            self.ns + 'set_parameters', ReconfigureSrv, self._set_callback)
 
     def update_configuration(self, changes):
         with self.mutex:
             new_config = copy.deepcopy(self.config)
             new_config.update(changes)
             self._clamp(new_config)
-            return self._change_config(new_config, self._calc_level(new_config, self.config))
+            return self._change_config(
+                new_config, self._calc_level(new_config, self.config))
 
     def _copy_from_parameter_server(self):
         for param in extract_params(self.type.config_description):
             try:
-                self.config[param['name']] = rospy.get_param(self.ns + param['name'])
+                self.config[param['name']] = rospy.get_param(
+                    self.ns + param['name'])
             except KeyError:
                 pass
 
@@ -104,16 +115,17 @@ class Server(object):
     def _change_config(self, config, level):
         self.config = self.callback(config, level)
         if self.config is None:
-            msg = 'Reconfigure callback should return a possibly updated configuration.'
+            msg = 'Reconfigure callback should ' + \
+                  'return a possibly updated configuration.'
             rospy.logerr(msg)
             raise DynamicReconfigureCallbackException(msg)
-        
+
         self._copy_to_parameter_server()
-        
+
         self.update_topic.publish(encode_config(self.config))
 
         return self.config
-   
+
     def _calc_level(self, config1, config2):
         level = 0
         for param in extract_params(self.type.config_description):
@@ -123,14 +135,15 @@ class Server(object):
         return level
 
     def _clamp(self, config):
-        for param in extract_params(self.type.config_description): 
-            maxval = self.type.max[param['name']] 
-            minval = self.type.min[param['name']] 
+        for param in extract_params(self.type.config_description):
+            maxval = self.type.max[param['name']]
+            minval = self.type.min[param['name']]
             val = config[param['name']]
-            if val > maxval and maxval != "": 
-                config[param['name']] = maxval 
-            elif val < minval and minval != "": 
-                config[param['name']] = minval 
+            if val > maxval and maxval != "":
+                config[param['name']] = maxval
+            elif val < minval and minval != "":
+                config[param['name']] = minval
 
     def _set_callback(self, req):
-        return encode_config(self.update_configuration(decode_config(req.config, self.type.config_description)))
+        return encode_config(self.update_configuration(
+            decode_config(req.config, self.type.config_description)))
